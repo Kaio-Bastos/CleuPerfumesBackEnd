a4d419ccba3b9c3279bfe7a664dcd0bc19216127
Kaio-Bastos-Alves <rm95196@estudante.fieb.edu.br>
2026-09-22 20:20:52 -0300

    localdate => string em Lote.java correção n.4

--- src/main/java/com/cleuperumes/CleuPerfumes/Model/Entity/Lote.java
+++ src/main/java/com/cleuperumes/CleuPerfumes/Model/Entity/Lote.java
@@ -1,18 +1,17 @@
 package com.cleuperumes.CleuPerfumes.Model.Entity;
 
-import java.time.LocalDate;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class Lote {
     private int quantidade;
-    private LocalDate validade;
+    private String validade;
 
     public Lote(){}
 
     public int getQuantidade() {return quantidade;}
     public void setQuantidade(int quantidade){this.quantidade = quantidade;}
 
-    public LocalDate getValidade(){return validade;}
-    public void setValidade(LocalDate validade){this.validade = validade;}
+    public String getValidade(){return validade;}
+    public void setValidade(String validade){this.validade = validade;}
 }
